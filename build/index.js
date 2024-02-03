b24c3d952ed7dcc7d8b3a07ab9eaed25aa246aef
Fandoy <fandyglitch3@gmail.com>
2024-02-03 16:56:32 +0800

    csa

--- build/index.js
+++ build/index.js
@@ -1,3 +1,3 @@
 export default async ({ req, res, log, error }) => {
-    return res.json({ message: "Hello world", param: req.path });
+    return res.json({ message: "Hello world", param: req });
 };
